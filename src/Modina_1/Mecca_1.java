395f15d7f3ad87b807169a46723ef26eae9414a7
saiful <saiful121968@gmail.com>
2018-01-05 15:13:04 -0500

    file added

---
+++ src/Modina_1/Mecca_1.java
@@ -0,0 +1,29 @@
+package Modina_1;
+
+
+public class Mecca_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		int a = 10, b = 15, c = 20;
+		
+		boolean result; 
+		 
+		
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c = " + c);
+
+		result = b > a;	
+		System.out.println("b > a =  " + result);
+		
+		System.out.println("b > c =  " + (b>c));
+		System.out.println("c > a =  " + (c>a));
+				
+		
+
+	}
+
+}
